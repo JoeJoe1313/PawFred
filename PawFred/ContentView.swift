e8dc77cf41fc605def43ac9b233f85cceffa3906
JoeJoe1313 <levcheva_joana@yahoo.com>
2024-07-17 01:01:45 +0300

    Initial Commit

---
+++ PawFred/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  PawFred
+//
+//  Created by Joana Levtcheva on 17/07/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//            Text("Hello, world!")
+            Text("Hi, I am Freddy!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.05, saturation: 0.653, brightness: 0.966))
+//            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+//                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+//            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PawFred/PawFredApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawFredApp.swift
+//  PawFred
+//
+//  Created by Joana Levtcheva on 17/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PawFredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
